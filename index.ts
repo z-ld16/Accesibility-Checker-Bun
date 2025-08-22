95e71fa96c8a184cefd90e35b96162dc6b4bec42
z-ld16 <zld3707@gmail.com>
2025-08-22 09:52:14 +0200

    feat: added config file

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import consola from 'consola'
 
+import { PORT } from './config/index.ts'
 import routes from './routes/index.ts'
 
 const app = express()
 app.use(express.json())
-const port = 3000
 
 app.use('/api', routes)
 
-app.listen(port, () => {
-  consola.log(`Listening on port ${port}...`)
+app.listen(PORT, () => {
+  consola.log(`Listening on port ${PORT}...`)
 })
